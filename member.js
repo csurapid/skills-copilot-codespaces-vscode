51090ad19f6e194d1c5e24a97565ae0c39f7b1b6
csurapid <128585374+csurapid@users.noreply.github.com>
2025-02-20 08:09:37 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+  var member = {
+    name: 'Jesse',
+    skills: ['JavaScript', 'React', 'Node'],
+    details: {
+      age: 25,
+      country: 'Canada'
+    }
+  };
+
+  return member;
+}
